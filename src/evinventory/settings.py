aff00d22dfa5ba0e7315dd9edf1ef1042fbbcb37
Evan Porter <evan.porter@dotman.ca>
2024-07-30 20:48:04 -0300

    Prefix app-specific secrets

--- src/evinventory/settings.py
+++ src/evinventory/settings.py
@@ -28,10 +28,10 @@ environ.Env.read_env(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('EVAN_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = env('EVAN_DEBUG')
 
 ALLOWED_HOSTS = []
 
