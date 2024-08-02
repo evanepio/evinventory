b54e0f4191daf58cea650681feacabfa19684908
Evan Porter <evan.porter@dotman.ca>
2024-08-02 16:54:33 -0300

    Use parameterization to reduce test code

--- tests/admin_test.py
+++ tests/admin_test.py
@@ -1,24 +1,21 @@
-from django.contrib.auth.models import User
+import pytest
 from django.test import Client
-
-
-def test_unauthenticated_user_lands_on_admin_page_get_redirected(client: Client):
-    response = client.get('/admin/')
-    assert response.status_code == 302
-
-def test_authenticated_admin_user_lands_on_admin_page_get_redirected(client: Client, django_user_model: User):
-    user = django_user_model.objects.create_user(username='donotcare', password='donotcare', is_staff=True)
-    client.force_login(user)
-    response = client.get('/admin/')
-    assert response.status_code == 200
-
-def test_authenticated_non_admin_user_lands_on_admin_page_get_redirected(client: Client, django_user_model: User):
-    user = django_user_model.objects.create_user(username='donotcare', password='donotcare', is_staff=False)
-    client.force_login(user)
-    response = client.get('/admin/')
-    assert response.status_code == 302
 
 
 def test_land_on_logon_page_get_200(client: Client):
     response = client.get('/admin/login/')
     assert response.status_code == 200
+
+
+@pytest.mark.parametrize("is_superuser,is_staff,expected", [
+    pytest.param(False, False, 302, id='superuser=False, staff=False, redirects'),
+    pytest.param(False, True, 200, id='superuser=False, staff=True, success'),
+    pytest.param(True, False, 302, id='superuser=True, staff=False, redirects'),
+    pytest.param(True, True, 200, id='superuser=True, staff=True, success'),
+])
+def test_admin_page_access(client: Client, django_user_model, is_superuser: bool, is_staff: bool, expected: int):
+    user = django_user_model.objects.create_user(username='donotcare', password='donotcare',
+                                                 is_superuser=is_superuser, is_staff=is_staff)
+    client.force_login(user)
+    response = client.get('/admin/')
+    assert response.status_code == expected
